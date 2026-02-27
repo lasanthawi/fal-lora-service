023f8a0761e45f8a26e29255dc38cb80a9e8f35c
lasanthawi <84735780+lasanthawi@users.noreply.github.com>
2026-02-27 13:25:04 +0530

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,22 @@
 import type { AppProps } from 'next/app';
+import { Suspense } from 'react';
 import { StackProvider } from '@stackframe/stack';
 import { stackServerApp } from '../stack.config';
+
+function LoadingFallback() {
+  return (
+    <div style={{ fontFamily: 'system-ui', display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
+      Loading…
+    </div>
+  );
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <StackProvider app={stackServerApp}>
-      <Component {...pageProps} />
+      <Suspense fallback={<LoadingFallback />}>
+        <Component {...pageProps} />
+      </Suspense>
     </StackProvider>
   );
 }
